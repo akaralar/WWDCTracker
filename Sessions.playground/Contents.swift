d9a2b3a9667e7f7ea25ca38d7ce511cc8bdd823e
Ahmet Karalar <akaralar@gmail.com>
2024-06-14 20:15:43 +0200

    Trim whitespace, add 2024

--- Sessions.playground/Contents.swift
+++ Sessions.playground/Contents.swift
@@ -15,22 +15,22 @@ var topicsById: [Int: String]
 let semaphore = DispatchSemaphore(value: 0)
 
 if useWeb {
-  let session = URLSession(configuration: .default)
-  let task = session.dataTask(with: URLRequest(url: url)) { data, _, _ in
-    guard let data = data else { return }
-    jsonData = data.decode(JSONData.self, dateDecodingStrategy: .iso8601)
-    semaphore.signal()
-  }
-  task.resume()
+    let session = URLSession(configuration: .default)
+    let task = session.dataTask(with: URLRequest(url: url)) { data, _, _ in
+        guard let data = data else { return }
+        jsonData = data.decode(JSONData.self, dateDecodingStrategy: .iso8601)
+        semaphore.signal()
+    }
+    task.resume()
 } else {
-  jsonData = Bundle.main.decode(JSONData.self, from: file)
-  semaphore.signal()
+    jsonData = Bundle.main.decode(JSONData.self, from: file)
+    semaphore.signal()
 }
 
 semaphore.wait()
 contents = jsonData.contents
 topicsById = jsonData.topics.reduce(into: [:], { result, next in
-  result[next.id] = next.name
+    result[next.id] = next.name
 })
 
 let printFormatter = DateFormatter()
@@ -44,67 +44,69 @@ let secondsFormatter = NumberFormatter()
 secondsFormatter.minimumIntegerDigits = 2
 
 func formattedDay(from date: Date?) -> String {
-  date.flatMap { printFormatter.string(from: $0) } ?? ""
+    date.flatMap { printFormatter.string(from: $0) } ?? ""
 }
 
 func formattedDuration(for durationInSeconds: Int?) -> String {
-  durationInSeconds.flatMap { durationFormatter.string(from: Double($0)) } ?? ""
+    durationInSeconds.flatMap { durationFormatter.string(from: Double($0)) } ?? ""
 }
 
 // The JSON doesn't include visionOS yet, so we add it manually if 'spatial' is mentioned
 func fixedPlatforms(title: String, topics: String, platforms: [String]) -> String {
-  var platforms = platforms
+    var platforms = platforms
 
-  if topics.lowercased().contains("spatial") || title.lowercased().contains("spatial") {
-    platforms.append("visionOS")
-  }
+    if topics.lowercased().contains("spatial") || title.lowercased().contains("spatial") {
+        platforms.append("visionOS")
+    }
 
-  return platforms.joined(separator: ", ")
+    return platforms.map { $0.trimmingCharacters(in: .whitespaces) }.joined(separator: ", ")
 }
 
 // The events we want to create csv files for
-let eventIDs = ["wwdc2020", "wwdc2021", "wwdc2022", "wwdc2023"]
+let eventIDs = ["wwdc2019", "wwdc2020", "wwdc2021", "wwdc2022", "wwdc2023", "wwdc2024"]
 
 // Some events like recap events etc include the following in their names, we will ignore them
 let filterWords = ["@WWDC21", "WWDC22", "WWDC23"]
 
-let header = "Session #; Title; Topics; Platforms; Date; Length; Link; Favourite; Watched; Uninterested;\n"
+let header = "Session #;Title;Topics;Platforms;Date;Length;Link;Favourite;Watched;Uninterested;\n"
 var output = eventIDs.reduce(into: [:], { result, next in result[next] = header })
 
-for item in contents where output[item.eventId] != nil && item.type == "Video" {
-  // If an item doesn't have a platform assigned and includes one of the filter words in title,
-  // we ignore and print to the console
-  if item.platforms == nil && !filterWords.allSatisfy({ !item.title.contains($0) }) {
-    print("Item not included: \(item.title) - \(item.webPermalink)")
-    continue
-  }
+for item in contents where output[item.eventId] != nil && (item.type == "Video" || item.type == "Session") {
+    // If an item doesn't have a platform assigned and includes one of the filter words in title,
+    // we ignore and print to the console
+    if item.platforms == nil && !filterWords.allSatisfy({ !item.title.contains($0) }) {
+        print("Item not included: \(item.title) - \(item.webPermalink)")
+        continue
+    }
 
-  guard var outputString = output[item.eventId] else { continue }
+    guard var outputString = output[item.eventId] else { continue }
 
-  outputString += "\(item.id); "
-  outputString += "\(item.title); "
-  let topics = item.topicIds.compactMap { topicsById[$0] }.joined(separator: ", ")
-  outputString += "\(topics); "
-  outputString += "\(fixedPlatforms(title: item.title, topics: topics, platforms: item.platforms ?? [])); "
-  outputString += "\(formattedDay(from: item.originalPublishingDate)); "
-  outputString += "\(formattedDuration(for: item.media?.duration)); "
-  outputString += "\(item.webPermalink); "
-  outputString += " ; ; ;\n"
+    outputString += "\(item.id.trimmingCharacters(in: .whitespaces));"
+    outputString += "\(item.title.trimmingCharacters(in: .whitespaces));"
+    let topics = item.topicIds.compactMap { topicsById[$0] }
+        .map { $0.trimmingCharacters(in: .whitespaces) }
+        .joined(separator: ", ")
+    outputString += "\(topics);"
+    outputString += "\(fixedPlatforms(title: item.title, topics: topics, platforms: item.platforms ?? []));"
+    outputString += "\(formattedDay(from: item.originalPublishingDate));"
+    outputString += "\(formattedDuration(for: item.media?.duration));"
+    outputString += "\(item.webPermalink);"
+    outputString += ";;;\n"
 
-  output[item.eventId] = outputString
+    output[item.eventId] = outputString
 }
 
 let outputFolder = PlaygroundSupport.playgroundSharedDataDirectory
 for (eventID, outputString) in output {
-  let outputURL = outputFolder.appendingPathComponent("\(eventID).csv")
+    let outputURL = outputFolder.appendingPathComponent("\(eventID).csv")
 
-  do {
-    try FileManager.default.createDirectory(at: outputFolder, withIntermediateDirectories: true, attributes: nil)
-    try outputString.write(toFile: outputURL.path, atomically: false, encoding: .utf8)
-  } catch {
-    print(error.localizedDescription)
-  }
+    do {
+        try FileManager.default.createDirectory(at: outputFolder, withIntermediateDirectories: true, attributes: nil)
+        try outputString.write(toFile: outputURL.path, atomically: false, encoding: .utf8)
+    } catch {
+        print(error.localizedDescription)
+    }
 
-  print("Open \(eventID).csv via:")
-  print("open", outputURL.path.replacingOccurrences(of: " ", with: "\\ "))
+    print("Open \(eventID).csv via:")
+    print("open", outputURL.path.replacingOccurrences(of: " ", with: "\\ "))
 }
--- Sessions.playground/Sources/Models.swift
+++ Sessions.playground/Sources/Models.swift
@@ -1,30 +1,30 @@
 import Foundation
 
 public struct JSONData: Decodable {
-  public let contents: [Video]
+    public let contents: [Video]
     public let topics: [Topic]
 }
 
 public struct Video: Decodable {
-  public let id: String
-  public let staticContentId: Int
-  public let eventContentId: Int
-  public let eventId: String
-  public let webPermalink: String
-  public let description: String
-  public let title: String
-  public let topicIds: [Int]
-  public let type: String
-  public let trackId: Int
-  public let startTime: String?
-  public let originalPublishingDate: Date?
-//  public let endTime: String = ""
-  public let media: Media?
-  public let platforms: [String]?
+    public let id: String
+    public let staticContentId: Int
+    public let eventContentId: Int
+    public let eventId: String
+    public let webPermalink: String
+    public let description: String
+    public let title: String
+    public let topicIds: [Int]
+    public let type: String
+    public let trackId: Int
+    public let startTime: String?
+    public let originalPublishingDate: Date?
+    //  public let endTime: String = ""
+    public let media: Media?
+    public let platforms: [String]?
 }
 
 public struct Media: Decodable {
-  public let duration: Int
+    public let duration: Int
 }
 
 public struct Topic: Decodable {
